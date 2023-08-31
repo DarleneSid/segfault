29cd319431716fbed96fc92194a099164175088e
pferreir42 <141176584+pferreir42@users.noreply.github.com>
2023-08-31 03:30:44 +0200

    Update ft_expand.c

--- minishell/srcs/ft_expand.c
+++ minishell/srcs/ft_expand.c
@@ -12,6 +12,16 @@
 
 #include "minishell.h"
 
+int	ft_strlen_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	return (i);
+}
+
 int	expand_replace(char **str, int start, char *replace)
 {
 	char	*new;
